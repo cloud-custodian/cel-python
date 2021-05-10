b5825400c3ab4ec53be27a566c59142480b06b07
kylejohnson514 <kyle.johnson@capitalone.com>
2021-05-10 14:11:38 -0400

    Make classifiers a list for future possible additions

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     version='0.1.4',
     description='Pure Python CEL Implementation',
     license='Apache-2.0',
-    classifiers='License :: OSI Approved :: Apache Software License',
+    classifiers=['License :: OSI Approved :: Apache Software License'],
     long_description=(here/"README.rst").read_text(),
     long_description_content_type='text/x-rst',
     author='Cloud Custodian Project',
